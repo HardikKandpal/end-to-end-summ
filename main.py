9c0134ab6e7932870e40003573aec71699fba6fc
Hardik_Kandpal <hardikkandpal705@gmail.com>
2024-10-18 15:07:10 +0530

    commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from textsumm.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from textsumm.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
 from textsumm.pipeline.stage_3_data_transformation import DataTransformationTrainingPipeline
+from textsumm.pipeline.stage_4_model_trainer import ModelTrainerTrainingPipeline
+
 
 
 from textsumm.logging import logger
@@ -31,6 +33,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_transformation = DataTransformationTrainingPipeline()
    data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
